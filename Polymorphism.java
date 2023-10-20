6f14ec18a7d740820ccb6ec76ac1feb689f88fbf
Ankit Chahal <50526396+ankitchahal20@users.noreply.github.com>
2023-10-20 20:55:11 +0530

    Create Polymorphism.java

---
+++ Polymorphism.java
@@ -0,0 +1,24 @@
+// Polymorphism
+
+/*
+
+Polymorphism : one name multiple forms is known as Polymorphism
+Poly means many and morphism means form.
+
+There are two types of Polymorphism,
+    - compile time polymorphism,
+    - runtime polymorphism,
+
+If the selection of one-form amongst the many form is performed at compile-time then there will be compile time
+polymorphism such as methodoverloading, operator overloading.
+
+If the selection of one-form amongst the many form is performed at run-time then there will be run time
+polymorphism.
+
+polymorphism
+    - compile-time polymorphism
+        - method overloading
+        - operator overloading
+    - run-time polymorphism
+        - method overriding
+*/
