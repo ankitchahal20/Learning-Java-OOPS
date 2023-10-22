ef18c7f7a2f172550e7b70b5b643cf50c3f6beba
Ankit Chahal <50526396+ankitchahal20@users.noreply.github.com>
2023-10-22 13:14:28 +0530

    Create MethodOverloadingWithDifferentNumOfArguments.java

---
+++ MethodOverloadingWithDifferentNumOfArguments.java
@@ -0,0 +1,34 @@
+/*
+
+Method Overloading : When there are more than one method with the same name, but with different argument list,
+this is known as the method overloading.
+
+The return type of the method never affects to the overloading.
+
+The argument list can be changed in the following ways,
+    - by changing the no. of arguments,
+    - by changing the type of arguments,
+    - by changing the sequence of arguments.
+    
+    
+ReturnType  methodName      (ArgumentList)
+may or may  must be same     must be changed.
+not change  
+*/
+
+class Test{
+    void m1(int x){
+        System.out.println("m1 with one argument");
+    }
+    void m1(int x, int y){
+        System.out.println("m1 with two argument");
+    }
+}
+
+class MethodOverloadingWithDifferentNumOfArguments {
+    public static void main(String args[]){
+        Test t=new Test();
+        t.m1(4);
+        t.m1(3,4);
+    }
+}
